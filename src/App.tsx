34e1aeabae65ca14cd68c8c2c871917b0fa19b1c
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-08-12 16:15:03 -0300

    fix: Ajustar rotas navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import DeletarServico from './components/servicos/deletarservico/DeletarServico'
+import FormServico from './components/servicos/formservico/FormServico'
+import ListaServicos from './components/servicos/listaservico/ListaServicos'
 import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import Navbar from './components/navbar/Navbar'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify'
-import ListaServicos from './components/servicos/listaservico/ListaServicos'
-import FormServico from './components/servicos/formservico/FormServico'
-import DeletarServico from './components/servicos/deletarservico/DeletarServico'
+import ListarCategoria from './components/categoria/listarcategoria/ListarCategoria'
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
                 <Route path='/' element={<Login />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/servicos' element={<ListaServicos />} />
+                <Route path='/categorias' element={<ListarCategoria />} />
                 <Route path="/cadastrarservico" element={<FormServico />} />
                 <Route path="/editarservico/:id" element={<FormServico />} />
                 <Route path="/deletarservico/:id" element={<DeletarServico />} />
