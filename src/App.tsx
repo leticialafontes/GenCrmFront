f7c801efdcfcc0c5e03c8b8128ae2bb52ca6fbde
Samuel de Souza <samuelsouzarosajr@gmail.com>
2025-08-12 09:07:08 -0300

    Criacao da branch dev

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,10 @@ function App() {
             <Navbar />
             <div className='min-h-[80vh]'>
               <Routes>
-                <Route path='/' element={<Login />} />
+                {/* <Route path='/' element={<Login />} />
                 <Route path='/home' element={<Home />} />
                 <Route path='/cadastro' element={<Cadastro />} />
-                <Route path='/login' element={<Login />} />
+                <Route path='/login' element={<Login />} /> */}
               </Routes>
             </div>
             <Footer />
