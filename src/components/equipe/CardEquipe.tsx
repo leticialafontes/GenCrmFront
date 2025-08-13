800eff75ad5acbdcdb92693f4f7f78b2cbb227df
Lari <larissateixeira998@gmail.com>
2025-08-12 23:27:02 -0300

    altera cards da equipe

--- src/components/equipe/CardEquipe.tsx
+++ src/components/equipe/CardEquipe.tsx
@@ -17,16 +17,21 @@ function CardEquipe({ devs }: SobreProps) {
                 scale: {visualDuration: 0.2 },
             }}
         className="flex flex-col bg-white shadow-lg rounded-2xl overflow-hidden justify-between w-2xs h-sm">
+        <div className="flex">
         <header className="py-2 px-2 bg-white">
-          <img src={devs.pic} className="rounded-2xl" />
+          <img src={devs.pic} className="rounded-full" />
         </header>
+        <div className="flex flex-col bg-white">
         <p className="px-4 py-2 text-2xl bg-white text-center font-bold">{devs.nome}</p>
         <p className="px-4 pb-4 text-1xl bg-white text-center">{devs.cargo}</p>
-        <div className="flex flex-col bg-white">
-          <a href={devs.linkedin} target="_blank" className="text-white bg-blue-600 hover:bg-blue-500 cursor-pointer p-1 flex items-center justify-center rounded-2xl m-1">
+        </div>
+        </div>
+
+        <div className="flex justify-evenly p-2">
+          <a href={devs.linkedin} target="_blank" className="text-white bg-blue-600 hover:bg-blue-500 cursor-pointer p-1 flex items-center justify-center rounded-2xl ">
             <LinkedinLogoIcon size={24} className="m-1" /> LinkedIn
           </a>
-          <a href={devs.github} target="_blank" className="text-white bg-black hover:bg-slate-600 cursor-pointer p-1 flex items-center justify-center rounded-2xl mx-1 my-2">
+          <a href={devs.github} target="_blank" className="text-white bg-black hover:bg-slate-600 cursor-pointer p-1 flex items-center justify-center rounded-2xl px-2">
             <GithubLogoIcon size={24} className="m-1" />GitHub
           </a>
         </div>
