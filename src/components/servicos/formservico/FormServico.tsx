b0dc2876410ae04f70404710c91951c298c1e90d
LuanaTechVet <luanaap2702@gmail.com>
2025-08-13 10:31:21 -0300

    fix: Form

--- src/components/servicos/formservico/FormServico.tsx
+++ src/components/servicos/formservico/FormServico.tsx
@@ -8,227 +8,250 @@ import { ToastAlerta } from "../../../utils/ToastAlerta";
 import { RotatingLines } from "react-loader-spinner";
 
 function FormServico() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [ isLoading, setIsLoading ] = useState<boolean>(false);
-    const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const [categoria, setCategoria] = useState<Categoria>({ id: 0, nome: "" });
+  const [servico, setServico] = useState<Servico>({} as Servico);
 
-    const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
-    const [servico, setServico] = useState<Servico>({} as Servico);
+  const { id } = useParams<{ id: string }>();
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
 
-    const { id } = useParams<{ id: string }>();
-
-        const { usuario, handleLogout } = useContext(AuthContext)
-        const token = usuario.token
-
-    async function buscarServicoPorId(id: string) {
-        try {
-        await buscar(`/servicos/${id}`, setServico, { 
-                headers: { Authorization: token }
-            });
-        } catch (error: any) {
-            if (error.toString().includes('401')) {
-                handleLogout()
-            }
-        }
+  async function buscarServicoPorId(id: string) {
+    try {
+      await buscar(`/servicos/${id}`, setServico, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        handleLogout();
+      }
     }
+  }
 
-    async function buscarCategoriaPorId(id: string) {
-        try {
-        await buscar(`/categorias/${id}`, setCategoria, { 
-                headers: { Authorization: token }
-            });
-        } catch (error: any) {
-            if (error.toString().includes('401')) {
-                handleLogout()
-            }
-        }
+  async function buscarCategoriaPorId(id: string) {
+    try {
+      await buscar(`/categorias/${id}`, setCategoria, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        handleLogout();
+      }
     }
+  }
 
-    async function buscarCategorias() {
-        try {
-        await buscar("/categorias", setCategorias, {
-                headers: { Authorization: token }
-            })
-        } catch (error: any) {
-            if (error.toString().includes('401')) {
-                handleLogout()
-            }
-        }
+  async function buscarCategorias() {
+    try {
+      await buscar("/categorias", setCategorias, {
+        headers: { Authorization: token },
+      });
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        handleLogout();
+      }
     }
+  }
 
-    useEffect(() => {
-        if (token === '') {
-            ToastAlerta("Você precisa estar logado", "erro");
-            navigate('/');
-        }
-    }, [navigate, token])
+  useEffect(() => {
+    if (token === "") {
+      ToastAlerta("Você precisa estar logado", "erro");
+      navigate("/");
+    }
+  }, [navigate, token]);
 
+  useEffect(() => {
+    buscarCategorias();
+    if (id !== undefined) {
+      buscarServicoPorId(id);
+    }
+  }, [id]);
 
-    useEffect(() => {
-        buscarCategorias();
+  useEffect(() => {
+    setServico({
+      ...servico,
+      categoria: categoria,
+    });
+  }, [categoria]);
 
-        if (id !== undefined) {
-        buscarServicoPorId(id);
-        }
-    }, [id]);
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setServico({
+      ...servico,
+      [e.target.name]: e.target.value,
+      categoria: categoria,
+      usuario: usuario,
+    });
+  }
 
-    useEffect(() => {
-        setServico({
-        ...servico,
-        categoria: categoria,
+  function retornar() {
+    navigate("/servicos");
+  }
+
+  async function gerarNovoServico(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+ console.log(servico)
+    if (id !== undefined) {
+      try {
+        await atualizar(`/servicos`, servico, setServico, {
+          headers: {
+            Authorization: token,
+          },
         });
-    }, [categoria]);
-
-    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-        setServico({
-        ...servico,
-        [e.target.name]: e.target.value,
-        categoria: categoria,
-        });
-    }
-
-    function retornar() {
-        navigate("/servicos");
-    }
-
-    async function gerarNovoServico(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setIsLoading(true);
-
-        if (id !== undefined) {
-            try {
-                await atualizar(`/servicos`, servico, setServico, {
-                        headers: {
-                            Authorization: token,
-                        },
-                    });
-                ToastAlerta("Serviço atualizado com sucesso!", "sucesso")
-
-            } catch (error: any) {
-                    if (error.toString().includes('401')) {
-                        handleLogout()
-                    } else {
-                        ToastAlerta("Erro ao atualizar o Serviço", "erro")
-                    }
-                }
+        ToastAlerta("Serviço atualizado com sucesso!", "sucesso");
+      } catch (error: any) {
+        if (error.toString().includes("401")) {
+          handleLogout();
         } else {
-            try {
-                await cadastrar(`/servicos`, servico, setServico, {
-                        headers: {
-                            Authorization: token,
-                        },
-                    });
-                ToastAlerta("Serviço cadastrado com sucesso!", "sucesso")
-            
-            } catch (error: any) {
-                if (error.toString().includes('401')) {
-                        handleLogout()
-                    } else {
-                        ToastAlerta("Erro ao cadastrar o Serviço", "erro")
-                    }
-            }
+          ToastAlerta("Erro ao atualizar o Serviço", "erro");
         }
-
-        setIsLoading(false);
-        retornar();
+      }
+    } else {
+      try {
+        await cadastrar(`/servicos`, servico, setServico, {
+          headers: {
+            Authorization: token,
+          },
+        });
+        ToastAlerta("Serviço cadastrado com sucesso!", "sucesso");
+      } catch (error: any) {
+        if (error.toString().includes("401")) {
+          handleLogout();
+        } else {
+          ToastAlerta("Erro ao cadastrar o Serviço", "erro");
+        }
+      }
     }
 
-    const carregandoCategoria = categoria.nome === "";
+    setIsLoading(false);
+    retornar();
+  }
 
-    return (
-        <div className="container flex flex-col mx-auto items-center popup-content bg-slate-700 text-white">
-        <h1 className="text-4xl text-center my-8">
-            {id !== undefined ? "Editar ServiÇo" : "Cadastrar ServiÇo"}
-        </h1>
+  const carregandoCategoria = categoria.nome === "";
 
-        <form className="flex flex-col w-1/2 gap-4" onSubmit={gerarNovoServico}>
+  return (
+    <div
+      id="authentication-modal"
+      tabIndex={-1}
+      aria-hidden="true"
+      className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 
+                 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    >
+      <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div className="relative rounded-lg shadow bg-sky-800 p-6">
+            <button
+                type="button"
+                onClick={retornar}
+                className="absolute top-4 right-4 text-white hover:text-gray-800 text-xl font-bold"
+                aria-label="Fechar"
+                >
+                ×
+            </button>
+
+          <h1 className="text-2xl font-bold text-center mb-6 text-stone-50">
+            {id !== undefined ? "Editar Serviço" : "Cadastrar Serviço"}
+          </h1>
+
+          <form className="flex flex-col gap-4" onSubmit={gerarNovoServico}>
             <div className="flex flex-col gap-2">
-            <label htmlFor="titulo">Nome do Serviço</label>
-            <input
+              <label htmlFor="nome">Nome do Serviço</label>
+              <input
                 type="text"
-                placeholder="Informe o nome do ServiÇo"
+                placeholder="Informe o nome do Serviço"
+                id="nome"
                 name="nome"
                 required
-                className="border-2 border-slate-700 rounded p-2"
-                value={servico.nome}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+                className="border-2 border-sky-950 rounded p-2"
+                value={servico.nome || ""}
+                onChange={atualizarEstado}
+              />
             </div>
+
             <div className="flex flex-col gap-2">
-            <label htmlFor="titulo">Descrição</label>
-            <input
+              <label htmlFor="descricao">Descrição</label>
+              <input
                 type="text"
                 placeholder="Descreva o Serviço"
+                id="descricao"
                 name="descricao"
                 required
                 className="border-2 border-slate-700 rounded p-2"
-                value={servico.descricao}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+                value={servico.descricao || ""}
+                onChange={atualizarEstado}
+              />
             </div>
+
             <div className="flex flex-col gap-2">
-            <label htmlFor="titulo">valor</label>
-            <input
+              <label htmlFor="valor">Valor</label>
+              <input
                 type="number"
                 placeholder="Informe o valor do Serviço"
+                id="valor"
                 name="valor"
                 required
                 className="border-2 border-slate-700 rounded p-2"
-                value={servico.valor}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+                value={servico.valor || 0}
+                onChange={atualizarEstado}
+              />
             </div>
+
             <div className="flex flex-col gap-2">
-            <label htmlFor="titulo">Status</label>
-            <input
+              <label htmlFor="status">Status</label>
+              <input
                 type="text"
                 placeholder="Informe o Status do Serviço"
+                id="status"
                 name="status"
                 required
                 className="border-2 border-slate-700 rounded p-2"
-                value={servico.status}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            />
+                value={servico.status || ""}
+                onChange={atualizarEstado}
+              />
             </div>
-            <div className="flex flex-col gap-2">
-            <p>Categoria do Servico</p>
-                <select
-                    name="categoria"
-                    id="categoria"
-                    className="border p-2 border-slate-800 rounded"
-                    onChange={(e) => buscarCategoriaPorId(e.currentTarget.value)}
-                >
-                    <option value="" selected disabled>
-                    Selecione o Categoria
-                    </option>
 
-                    {categorias.map((categoria) => (
-                    <>
-                        <option value={categoria.id}>{categoria.nome}</option>
-                    </>
-                    ))}
-                </select>
+            <div className="flex flex-col gap-2">
+              <p>Categoria do Serviço</p>
+              <select
+                name="categoria"
+                id="categoria"
+                className="border p-2 border-slate-800 rounded"
+                onChange={(e) => buscarCategoriaPorId(e.currentTarget.value)}
+              >
+                <option value="" disabled selected>
+                  Selecione a Categoria
+                </option>
+                {categorias.map((cat) => (
+                  <option key={cat.id} value={cat.id}>
+                    {cat.nome}
+                  </option>
+                ))}
+              </select>
             </div>
+
             <button
-            type="submit"
-            className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center"
-            disabled={carregandoCategoria}
+              type="submit"
+              className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center"
+              disabled={carregandoCategoria}
             >
-            {isLoading ?
-                        <RotatingLines
-                            strokeColor="white"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="24"
-                            visible={true}
-                        /> :
-            <span>{id !== undefined ? "Atualizar" : "Cadastrar"}</span>
-            }
+              {isLoading ? (
+                <RotatingLines
+                  strokeColor="white"
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  width="24"
+                  visible={true}
+                />
+              ) : (
+                <span>{id !== undefined ? "Atualizar" : "Cadastrar"}</span>
+              )}
             </button>
-        </form>
+          </form>
         </div>
-    );
-    }
+      </div>
+    </div>
+  );
+}
 
 export default FormServico;
--- src/components/servicos/modalservico/ModalServico.tsx
+++ src/components/servicos/modalservico/ModalServico.tsx
@@ -3,18 +3,24 @@ import FormServico from "../formservico/FormServico";
 
 function ModalServico() {
   return (
-    <>
-      <Popup
-        trigger={
-          <button className="border rounded px-4 py-2 hover:bg-white hover:text-indigo-800">
-            Novo ServiÇo
-          </button>
-        }
-        modal
-      >
-        <FormServico />
-      </Popup>
-    </>
+    <Popup
+      trigger={
+        <button
+          data-modal-target="authentication-modal"
+          data-modal-toggle="authentication-modal"
+          className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 
+                     focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
+                     text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
+                     dark:focus:ring-blue-800"
+          type="button"
+        >
+          Novo Serviço
+        </button>
+      }
+      modal
+    >
+      <FormServico />
+    </Popup>
   );
 }
 
