83fe3768f8cc3f5f022600477de1782d83513952
Lari <larissateixeira998@gmail.com>
2025-08-12 23:42:02 -0300

    estiliza página da equipe

--- src/components/equipe/Equipe.tsx
+++ src/components/equipe/Equipe.tsx
@@ -5,9 +5,9 @@ function Equipe() {
   const equipe = devs;
   return (
     <>
-    <div id="equipe" className="mt-auto flex justify-center w-full bg-neutral-300 pb-10">
+    <div id="equipe" className="mt-auto flex justify-center w-full bg-[#EDF2F4] pb-10">
         <div className="container w-[80vw] flex flex-col ">
-          <span className="block text-red-800 text-5xl font-bold text-left py-10 ">
+          <span className="block text-[#325E80] text-5xl font-bold py-10 ml-10">
                         EQUIPE
                     </span>
           <div className="flex flex-wrap flex-row justify-center gap-12 ">
